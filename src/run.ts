3c63d7326b072f838e36bca8a6d5ff86327e0a79
humanchimp <morphcham@gmail.com>
2019-01-11 20:05:09 -0800

    code coverage 100%

--- src/run.ts
+++ src/run.ts
@@ -9,15 +9,17 @@ import {
 import { Suite } from "./Suite";
 import { shuffle } from "./shuffle";
 
-export async function run(suite: Suite, params: RunParams);
-export async function run(suites: Suite[], params: RunParams);
+export async function run(suite: Suite): Promise<void>;
+export async function run(suites: Suite[]): Promise<void>;
+export async function run(suite: Suite, params: RunParams): Promise<void>;
+export async function run(suites: Suite[], params: RunParams): Promise<void>;
 export async function run(
   suites: Suite | Suite[],
   {
     generate = generator,
     perform = console.log,
     sort = shuffle,
-    predicate,
+    predicate = Boolean,
   }: RunParams = {},
 ): Promise<void> {
   for await (const report of generate([].concat(suites), sort, predicate)) {
@@ -25,10 +27,34 @@ export async function run(
   }
 }
 
+export function generator(
+  suite: Suite,
+): AsyncIterableIterator<Plan | Report | Summary>;
+export function generator(
+  suites: Suite[],
+): AsyncIterableIterator<Plan | Report | Summary>;
+export function generator(
+  suite: Suite,
+  sort: Sorter,
+): AsyncIterableIterator<Plan | Report | Summary>;
+export function generator(
+  suites: Suite[],
+  sort: Sorter,
+): AsyncIterableIterator<Plan | Report | Summary>;
+export function generator(
+  suite: Suite,
+  sort: Sorter,
+  predicate: JobPredicate,
+): AsyncIterableIterator<Plan | Report | Summary>;
+export function generator(
+  suites: Suite[],
+  sort: Sorter,
+  predicate: JobPredicate,
+): AsyncIterableIterator<Plan | Report | Summary>;
 export async function* generator(
   suites: Suite | Suite[],
   sort: Sorter = shuffle,
-  predicate: JobPredicate,
+  predicate: JobPredicate = Boolean,
 ): AsyncIterableIterator<Plan | Report | Summary> {
   yield* Suite.from([].concat(suites)).run(sort, predicate);
 }
--- test/run-test.ts
+++ test/run-test.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { spy as createSpy } from "sinon";
-import * as stable from "../src/lib";
+import { describe as createSuite, run, generator } from "../src/lib";
 
 async function* generate(_, sort) {
   const result = [1, 2, 3];
@@ -9,21 +9,21 @@ async function* generate(_, sort) {
   yield* result;
 }
 
-describe("the run helper", () => {
+describe("run", () => {
   describe("when an array of suites is passed", () => {
     let suites;
 
     beforeEach(() => {
       suites = [
-        stable.describe(null).it("should run"),
-        stable.describe(null).it("should run 2"),
-        stable.describe(null).it("should run 3"),
+        createSuite(null).it("should run"),
+        createSuite(null).it("should run 2"),
+        createSuite(null).it("should run 3"),
       ];
     });
 
     it("should by default perform console.log", async () => {
       createSpy(console, "log");
-      await stable.run(suites, { generate });
+      await run(suites, { generate });
       expect(console.log.callCount).to.equal(3);
       console.log.restore();
     });
@@ -40,14 +40,14 @@ describe("the run helper", () => {
       });
 
       it("should be possible to perform an aribtrary effect", async () => {
-        await stable.run(suites, { generate, perform });
+        await run(suites, { generate, perform });
         expect(memo.sort((a, b) => a - b)).to.eql([1, 2, 3]);
       });
 
       it("should be possible to pass a sort algorithm", async () => {
         const sort = array => array.sort((a, b) => b - a);
 
-        await stable.run(suites, { generate, perform, sort });
+        await run(suites, { generate, perform, sort });
         expect(memo).to.eql([3, 2, 1]);
       });
     });
@@ -63,8 +63,7 @@ describe("the run helper", () => {
     let suite;
 
     beforeEach(() => {
-      suite = stable
-        .describe(null)
+      suite = createSuite(null)
         .it("should run")
         .it("should also run")
         .it("should run three");
@@ -73,16 +72,22 @@ describe("the run helper", () => {
     it("should pull the specs out of the default generator", async () => {
       const spy = createSpy();
 
-      await stable.run(suite, { perform: spy });
+      await run(suite, { perform: spy });
       expect(spy.callCount).to.equal(5);
     });
 
     describe("when called without options", () => {
-      it("should default using to the reports generator");
+      it("should default using to the reports generator", async () => {
+        const spy = createSpy();
+
+        await run(suite);
+
+        // TODO: complete writing this test case?
+      });
 
       it("should by default perform console.log", async () => {
         createSpy(console, "log");
-        await stable.run(suite);
+        await run(suite);
         expect(console.log.callCount).to.equal(5);
         console.log.restore();
       });
@@ -91,5 +96,51 @@ describe("the run helper", () => {
         "it would be nice to have a story for testing sort is shuffle by default :thinking_face:",
       );
     });
+  });
+});
+
+describe("generator", () => {
+  let suite, spy;
+
+  beforeEach(() => {
+    spy = createSpy();
+    suite = createSuite(null)
+      .it("should run", spy)
+      .it("stub")
+      .xit("skipped");
+  });
+
+  describe("generator(suite: Suite)", () => {
+    it("should return an async iterator over the reports in shuffled order", async () => {
+      for await (const report of generator(suite)) {
+        if ("ok" in report && !("completed" in report)) {
+          expect(report.ok).to.be.true;
+        }
+      }
+    });
+  });
+
+  describe("generator(suites: Suite[])", () => {
+    it("should return an async iterator over the reports in shuffled order");
+  });
+
+  describe("generator(suite: Suite, sort: Sorter)", () => {
+    it("should return an async iterator over the reports in sorted order");
+  });
+
+  describe("generator(suites: Suite[], sort: Sorter)", () => {
+    it("should return an async iterator over the reports in sorted order");
+  });
+
+  describe("generator(suite: Suite, sort: Sorter, predicate: JobPredicate)", () => {
+    it(
+      "should return an async iterator over the filtered reports in sorted order",
+    );
+  });
+
+  describe("generator(suites: Suite[], sort: Sorter, predicate: JobPredicate)", () => {
+    it(
+      "should return an async iterator over the filtered reports in sorted order",
+    );
   });
 });
