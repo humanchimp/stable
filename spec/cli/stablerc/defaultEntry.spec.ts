a3677b59f61aef7741bbaf80fa867eb8fdbe92cf
humanchimp <morphcham@gmail.com>
2019-02-25 21:19:27 -0800

    delete defaultEntry

--- spec/cli/stablerc/defaultEntry.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect } from "chai";
-import { defaultEntry } from "../../../src/cli/stablerc/defaultEntry";
-
-it("should append .stablerc to the given cwd", () => {
-  expect(defaultEntry("foo")).to.equal("foo/.stablerc");
-});
--- src/cli/stablerc/defaultEntry.ts
+++
@@ -1,5 +0,0 @@
-import { join } from "path";
-
-export function defaultEntry(cwd: string): string {
-  return join(cwd, ".stablerc");
-}
